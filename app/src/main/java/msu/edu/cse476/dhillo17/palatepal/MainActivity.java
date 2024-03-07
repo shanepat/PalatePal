1ccf78fdb4ea1dd283cad9f7ca7afc70c4088d36
mannandhillon <dhillo17@msu.edu>
2024-03-06 20:49:05 -0500

    Initial commit

---
+++ app/src/main/java/msu/edu/cse476/dhillo17/palatepal/MainActivity.java
@@ -0,0 +1,14 @@
+package msu.edu.cse476.dhillo17.palatepal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
